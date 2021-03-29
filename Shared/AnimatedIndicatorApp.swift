be636ec60f07a7e6cf35f9289e71a488bbc14f45
Elechim01 <mikymanny015@gmail.com>
2021-03-29 19:47:43 +0200

    Initial Commit

---
+++ Shared/AnimatedIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedIndicatorApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
